aebe177c035a5909473c643f5f43185ae5af527a
clement <maboiteaspam@wanadoo.fr>
2014-10-10 00:00:32 +0800

    improved unexpected socket disconnection sequence to avoid some errors and repeat

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -22,11 +22,6 @@ angular.module('wircApp')
     };
     $scope.login = function(){
         var w = wirc.get($scope.input.server_address);
-        w.onclose(function(){
-            $scope.user.logged = $scope.input.logged = false;
-            $scope.user.username = "";
-            $scope.user.token = "";
-        });
         w.open(function(){
             w.one("login_success",function(res){
                 $scope.$apply(function () {
@@ -35,15 +30,20 @@ angular.module('wircApp')
                     $scope.user.token = res.token;
                     $rootScope.$broadcast("user_login", $scope.user, w );
                 });
+                w.onclose(function(){
+                    $scope.$apply(function () {
+                        $rootScope.$broadcast("user_logout", $scope.user, w, true)
+                    });
+                });
             },true);
             w.login($scope.input.username);
         });
     };
-    $rootScope.$on("user_logout", function(ev, user, w){
-        w.quit(user.username, user.token)
+    $rootScope.$on("user_logout", function(ev, user, w, has_disconnected){
         $scope.user.logged = $scope.input.logged = user.logged = false;
         $scope.user.username = user.username = "";
         $scope.user.token = user.token = "";
+        if(!has_disconnected) w.quit(user.username, user.token)
     })
 
   });
