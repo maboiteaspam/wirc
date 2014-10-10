29a13f72ccb48fdd17a7c649c6ba07d535f81cde
clement <maboiteaspam@wanadoo.fr>
2014-10-10 23:57:44 +0800

    emit and receive new chat messages

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -8,36 +8,61 @@
  * Controller of the wircApp
  */
 angular.module('wircApp')
-  .controller('ChatCtrl', function ($scope) {
+  .controller('ChatCtrl', function ($scope, moment) {
     /* those are messages exchanged and broadcasted by users */
     $scope.messages = [
-        {
-            username:'',
-            text:'hello !'
-        }
+        /*
+         {
+         username:'xxx',
+         text:'hello !',
+         message_date:1,
+         id:0
+         }
+         */
     ];
     /* those are messages typed in by current user but not yet broadcasted to all users */
     $scope.local_messages = [
-        {
-            username:'',
-            text:'hello !'
-        }
+        /*
+         {
+         username:'yyyy',
+         text:'hello !',
+         message_date:1,
+         id:1
+         }
+         */
     ];
+    $scope.message_id = 0;
     /* when user sends a message */
-    $scope.$on("new_message",function($event,message){
-        $scope.local_messages.push({
-            text:message.text
-        })
-        /* scroll down on new message only if the scroll bar is already at bottom */
-        var el = angular.element(".chat");
-        if (el[0].offsetHeight + el.scrollTop() >= el[0].scrollHeight) {
-            el.scrollToElementAnimated(el.find("li").last());
-        }
+    $scope.$on("new_message",function($event, message, user, w){
+        var user_message = {
+            text:message,
+            username:user.username,
+            message_date:moment().valueOf(),
+            id: $scope.message_id
+        };
+        $scope.message_id++;
+        $scope.local_messages.push(user_message);
+        /* now broadcast to other users via central server */
+        w.send_message(user_message.username,user_message.text,user_message.id,user_message.message_date,user.token);
     })
-    /* when new messages income
-    WebSocket.onmessage(function(event) {
-        if( event.message == "new_message" ){
-            console.log('message: ', event.data);
-        }
-    });*/
+    /* when server emits a message */
+    $scope.$on("user_login",function($event, user, w){
+        w.on("message_sent",function(evt){
+            $scope.$apply(function () {
+                var message = evt.user_message;
+                if( message.username == user.username ){
+                    $scope.local_messages = $scope.reject($scope.local_messages, function(m){
+                        return m.id == message.message_id;
+                    })
+                }
+                var user_message = {
+                    text:message.user_message,
+                    username:message.username,
+                    message_date:message.message_date,
+                    id: message.message_id
+                };
+                $scope.messages.push(user_message);
+            });
+        })
+    });
   });
