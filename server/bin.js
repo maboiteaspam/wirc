7f3ee31f53e4404bbe5222e24e4fdc6a5315b12f
clement <maboiteaspam@wanadoo.fr>
2014-10-11 21:59:16 +0800

    code refactoring broadcasts user_leave and user_enter new events

--- server/bin.js
+++ server/bin.js
@@ -47,69 +47,83 @@ var UserHelper = function(){
         return false;
     }
 }
-function broadcast(server, msg) {
-    server.clients.forEach(function (client) {
-        client.send(msg)
-    })
-}
 var UserH = new UserHelper();
 wss.on('connection', function(ws) {
     console.log("connected");
-    ws.on('message', function(message) {
-        var data = JSON.parse(message);
-        if( data.message == "login" ){
-            var user = UserH.login(data.username);
-            if( user ){
-                ws.send(JSON.stringify({
-                    message:"login_success",
-                    token: user.token
-                }));
-                ws.on('close', function() {
-                    UserH.remove(user.username);
-                    console.log("disconnected : " + user.username);
-                });
-                console.log("successful login : " + user.username);
-                console.log("successful login : " + user.token);
-            }else{
-                ws.send(JSON.stringify({
-                    message:"login_failure"
-                }));
-                console.log("failure login : " + data.username);
+
+    /* websocket support */
+    var broadcast = function(msg){
+        wss.clients.forEach(function (client) {
+            client.send( JSON.stringify(msg) );
+        })
+    };
+    var emit = function(msg){
+        ws.send( JSON.stringify(msg) );
+    };
+    var on_message = function(m,h){
+        ws.on('message', function(raw_message) {
+            var data = JSON.parse(raw_message);
+            if( data.message == m ){
+                h(raw_message);
             }
+        });
+    };
+
+    /* app specific implementation */
+    on_message("login",function(data){
+        var user = UserH.login(data.username);
+        if( user ){
+            emit({
+                message:"login_success",
+                token: user.token
+            });
+            broadcast({
+                message:"user_enter",
+                username: data.username
+            });
+            ws.on('close', function() {
+                UserH.remove(user.username);
+                console.log("disconnected : " + user.username);
+            });
+            console.log("successful login : " + user.username);
+            console.log("successful login : " + user.token);
+        }else{
+            emit({
+                message:"login_failure"
+            });
+            console.log("failure login : " + data.username);
         }
     });
-    ws.on('message', function(message) {
-        var data = JSON.parse(message);
-        if( data.message == "send_message" ){
-            var user_message = {
-                username:data.username,
-                user_message:data.user_message,
-                message_id:data.message_id,
-                message_date:data.message_date
-            };
-            if( UserH.emit_message(data.username , data.token, user_message) ){
-                console.log("successful send_message : " + data.username);
-                broadcast(wss, JSON.stringify({
-                    message:"message_sent",
-                    user_message: user_message
-                }));
-            }else{
-                console.log("failure send_message : " + data.username);
-            }
+    on_message("send_message",function(data){
+        var user_message = {
+            username:data.username,
+            user_message:data.user_message,
+            message_id:data.message_id,
+            message_date:data.message_date
+        };
+        if( UserH.emit_message(data.username , data.token, user_message) ){
+            console.log("successful send_message : " + data.username);
+            broadcast({
+                message:"message_sent",
+                user_message: user_message
+            });
+        }else{
+            console.log("failure send_message : " + data.username);
         }
     });
-    ws.on('message', function(message) {
-        var data = JSON.parse(message);
-        if( data.message == "bye" ){
-            if( UserH.logout(data.username , data.token) ){
-                console.log("successful bye : " + data.username);
-                console.log("successful bye : " + data.token);
-            }else{
-                console.log("failure bye : " + data.username);
-                console.log("failure bye : " + data.token);
-            }
-            ws.close();
+    on_message("bye",function(data){
+        if( UserH.logout(data.username , data.token) ){
+            broadcast({
+                message:"user_leave",
+                username: data.username
+            });
+            console.log("successful bye : " + data.username);
+            console.log("successful bye : " + data.token);
+        }else{
+            console.log("failure bye : " + data.username);
+            console.log("failure bye : " + data.token);
         }
+        ws.close();
     });
 });
 console.log("ready ");
